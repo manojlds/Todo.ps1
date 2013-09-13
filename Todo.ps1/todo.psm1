fff73cc0b77f99a944edf77b50c75d8acc2a8680
Manoj Mahalingam <manojlds@gmail.com>
2013-09-14 00:56:55 +0530

    Using a module file

---
+++ Todo.ps1/todo.psm1
@@ -0,0 +1,11 @@
+﻿#Requires -Version 2.0
+Set-StrictMode -Version 2.0
+
+function t {
+	[CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Mandatory = 0)][switch] $help
+        )
+}
+ 
+Export-ModuleMember -function t
