34e28b86560f1e153ab1a08c4ec95468e984c975
Manoj Mahalingam <manojlds@gmail.com>
2013-09-14 01:53:27 +0530

    List terms filtering

--- Todo.ps1/todo.psm1
+++ Todo.ps1/todo.psm1
@@ -9,37 +9,46 @@ function Write-Help {
     Write-Output "todo.ps1 help"
 }
 
+function Create-TodoItem($todo, $number) {
+    $priority = "ZZ";
+    if($_ -match "^\(([A-Z])\) ") {
+        $priority = $matches[1]
+    }
+    $todoItem = New-Object PSObject -Property @{
+        "Todo" = $todo;
+        "Priority" = $priority;
+        "Line" = $number;
+    }
+
+    $todoItem
+}
+
 function t {
 	[CmdletBinding()]
     param(
         [Parameter(Mandatory = 0)][switch] $help,
         [Parameter(Position = 0, Mandatory = 0)][string] $command,
-        [Parameter(Position = 1, Mandatory = 0)][string[]] $args
+        [Parameter(Position = 1, Mandatory = 0)][string[]] $commandArgs
         )
-
     if($help){
         Write-Help
     }
 
     if($command -eq "add") {
-        $args[0] | Out-File -FilePath $TODO_TXT -Append -Encoding utf8
+        $commandArgs[0] | Out-File -FilePath $TODO_TXT -Append -Encoding utf8
     }
 
     if($command -eq "ls") {
-        $lineCount = 0;
+        $lineCount = 0
         Get-Content $TODO_TXT | %{
-            $lineCount++;
-            $priority = "ZZ";
-            if($_ -match "^\(([A-Z])\) ") {
-                $priority = $matches[1]
-            }
-            $todoItem = New-Object PSObject -Property @{
-                "Todo" = "$_";
-                "Priority" = $priority;
-                "Line" = $lineCount;
-            }
+            $lineCount++
+            $todo = $_
 
-            $todoItem
+            $filtered = $commandArgs | ?{ $todo -match $_; }
+
+            if(!$commandArgs -or $filtered) {
+                Create-TodoItem $todo $lineCount
+            }
 
         } | sort-object Priority | %{
             Write-Output ("{0} {1}" -f ($_.Line, $_.Todo))
