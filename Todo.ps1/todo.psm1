d0d9ab60a33f98a215d668754b7eabafce758bb0
Manoj Mahalingam <manojlds@gmail.com>
2013-09-14 01:14:27 +0530

    Implementing add command

--- Todo.ps1/todo.psm1
+++ Todo.ps1/todo.psm1
@@ -1,11 +1,31 @@
 ﻿#Requires -Version 2.0
 Set-StrictMode -Version 2.0
 
+$TODO_DIR = $PSScriptRoot
+$TODO_TXT = Join-Path $TODO_DIR todo.txt
+
+
+function Write-Help {
+    Write-Output "todo.ps1 help"
+}
+
 function t {
 	[CmdletBinding()]
     param(
-        [Parameter(Position = 0, Mandatory = 0)][switch] $help
+        [Parameter(Mandatory = 0)][switch] $help,
+        [Parameter(Position = 0, Mandatory = 0)][string] $command,
+        [Parameter(Position = 1, Mandatory = 0)][string[]] $args
         )
+
+    if($help){
+        Write-Help
+    }
+
+    if($command -eq "add") {
+        $args[0] | Out-File -FilePath $TODO_TXT -Append -Encoding utf8
+    }
+
+
 }
  
 Export-ModuleMember -function t
